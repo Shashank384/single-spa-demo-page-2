322e0078500edead345fb6bc8656825597f56697
Shashank384 <shashank.mayur@gmail.com>
2020-11-12 15:47:47 +0530

    page 2 - about - commit

---
+++ src/root.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container2">
+      <p>This project is based on micro frontend Architecture.</p>
+      <p>Single SPA framework is used to achieve the micro frontend architecture</p>
+    </div>
+  );
+}
